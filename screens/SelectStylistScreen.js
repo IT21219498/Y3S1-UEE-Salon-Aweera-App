7856e0824381d9c9568982c0259023fd217c78a3
IT21209420 <99248793+IT21209420@users.noreply.github.com>
2023-10-20 00:57:46 +0530

    Select Stylist Screen commit

---
+++ screens/SelectStylistScreen.js
@@ -0,0 +1,52 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React, { useContext } from 'react';
+import Header from '../components/Header';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import StylistCard from '../components/StylistCard';
+import { Appointment } from '../context/AppointmentContext';
+
+const SelectStylistScreen = () => {
+  const navigation = useNavigation();
+  const { appointmentDetails, setAppointmentDetails } = useContext(Appointment);
+  return (
+    <View>
+      <Header title={'Make an Appointment'} />
+      <View
+        style={{
+          alignItems: 'left',
+          marginTop: 15,
+          marginLeft: 5,
+          flexDirection: 'row',
+        }}
+      >
+        <Ionicons
+          onPress={() => {
+            navigation.goBack();
+            setAppointmentDetails((prevDetails) => prevDetails.slice(0, -1));
+          }}
+          style={{ marginRight: 5 }}
+          name="arrow-back-outline"
+          size={34}
+          color="Black"
+        />
+        <Text style={{ fontSize: 25, fontWeight: 'bold' }}>
+          Select a Stylist
+        </Text>
+      </View>
+      <View
+        style={{
+          borderWidth: 0.5,
+          borderColor: 'black',
+          marginTop: 15,
+          marginBottom: 10,
+        }}
+      />
+      <StylistCard />
+    </View>
+  );
+};
+
+export default SelectStylistScreen;
+
+const styles = StyleSheet.create({});
