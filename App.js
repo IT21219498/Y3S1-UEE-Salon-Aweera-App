be435b19d75420d79ee2d87ae1055ae7d1f09483
IT21209420 <99248793+IT21209420@users.noreply.github.com>
2023-10-20 00:55:16 +0530

    App commit

--- App.js
+++ App.js
@@ -1,14 +1,85 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import StackNavigator from "./StackNavigator";
-import { UserContext } from "./context/UserContext";
+import AppStack from "./navigation/AppStack";
+import { UserContext, UserType } from "./context/UserContext";
+import "react-native-gesture-handler";
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import CheckuserLogin from "./screens/CheckuserLogin";
+import {
+  useFonts,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+  Poppins_800ExtraBold,
+  Poppins_900Black,
+} from "@expo-google-fonts/poppins";
+import { NavigationContainer } from "@react-navigation/native";
+import AuthStack from "./navigation/AuthStack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AppointmentContext } from "./context/AppointmentContext";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+    Poppins_900Black,
+  });
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  const getAuthToken = async () => {
+    try {
+      const token = await AsyncStorage.getItem("authToken");
+      return token; // Return the token or null if it doesn't exist
+    } catch (err) {
+      console.log("Error", err);
+      return null;
+    }
+  };
+
+  // useEffect(() => {
+  //   const checkLoginStatus = async () => {
+  //     try {
+  //       const token = await getAuthToken();
+  //       if (token) {
+  //         setIsAuthenticated(false); // Set login status to true if token exists
+  //       }
+  //     } catch (err) {
+  //       console.log("Error", err);
+  //     }
+  //   };
+  //   console.log("isAuthenticated", isAuthenticated);
+
+  //   checkLoginStatus();
+  // }, []);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <>
-      <UserContext>
-        <StackNavigator />
-      </UserContext>
+      <NavigationContainer>
+        <UserContext>
+          <AppointmentContext>
+            {/* <AppStack /> */}
+            {/* <AuthStack /> */}
+            <CheckuserLogin />
+          </AppointmentContext>
+        </UserContext>
+      </NavigationContainer>
     </>
   );
 }
