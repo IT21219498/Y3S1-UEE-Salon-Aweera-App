105f881a25a0cb1bc4845e98e82984df5b8c1b5b
IT21209420 <99248793+IT21209420@users.noreply.github.com>
2023-10-20 01:38:17 +0530

    Appointment Screen Implemented

--- screens/AppointmentScreen.js
+++ screens/AppointmentScreen.js
@@ -1,10 +1,37 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+} from 'react-native';
+import React, { useContext, useEffect, useLayoutEffect, useState } from 'react';
+import Header from '../components/Header';
+import { FontAwesome } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import CategoryCards from '../components/CategoryCards';
+import { Appointment } from '../context/AppointmentContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const AppointmentScreen = () => {
+  const isAppointment = true;
   return (
-    <View>
-      <Text>AppointmentScreen</Text>
+    <View style={{ backgroundColor: '#F7F0FC', height: 1000 }}>
+      <Header title={'Make an Appointment'} />
+      <View style={{ alignItems: 'left', marginTop: 15, marginLeft: 15 }}>
+        <Text style={{ fontSize: 25, fontWeight: 'bold' }}>
+          Select a category
+        </Text>
+      </View>
+      <View
+        style={{
+          borderWidth: 0.5,
+          borderColor: 'black',
+          marginTop: 15,
+          marginBottom: 10,
+        }}
+      />
+      <CategoryCards isAppointment={isAppointment} />
     </View>
   );
 };
