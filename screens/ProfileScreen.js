373294d0ee0344d01d9af6a201c8f406e3e66c10
Chamikara Vithanage <it21219498@my.sliit.lk>
2023-10-20 01:41:28 +0530

    Profile Screen implemented

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,28 +1,64 @@
-import { StyleSheet, Text, View, Image, Pressable } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Pressable,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
 import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { UserType } from "../context/UserContext";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useNavigation } from "@react-navigation/native";
+import Header from "../components/Header";
+import * as ImagePicker from "expo-image-picker";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Alert } from "react-native";
 
 const ProfileScreen = () => {
-  const [user, setUser] = useState("");
-  const { userId, setUserId } = useContext(UserType);
+  const { loginUser, setLoginUser, userId, setUserId } = useContext(UserType);
+  const { user, setUser } = useContext(UserType);
   const navigation = useNavigation();
-  useEffect(() => {
-    const fetchProfile = async () => {
-      try {
-        const res = await axios.get(
-          `http://192.168.1.6:5000/profile/${userId}`
-        );
-        const { user } = res.data;
-        setUser(user);
-      } catch (err) {
-        console.log(err);
-      }
+
+  const [image, setImage] = useState(null);
+  const [name, setName] = useState("");
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  const editProfile = () => {
+    setName(user?.name);
+    const user = {
+      name: name,
+      ProfilePicture: image,
     };
-    fetchProfile();
-  }, []);
+    axios
+      .put(`http://192.168.1.25:5000/update-user/${userId}`, user)
+      .then((res) => {
+        Alert.alert("User Updated Successfully");
+        setName("");
+        setImage("");
+      })
+      .catch((err) => {
+        Alert.alert("User update failed", "Something went wrong");
+        console.log("Error", err);
+      });
+  };
 
   const logout = () => {
     clearAuthToken();
@@ -31,96 +67,150 @@ const ProfileScreen = () => {
   const clearAuthToken = async () => {
     await AsyncStorage.removeItem("authToken");
 
+    setLoginUser(null);
+
     console.log("token removed");
 
     navigation.replace("Login");
   };
   return (
-    <View style={{ marginTop: 55, padding: 15, backgroundColor: "white" }}>
-      <View>
-        <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
-          <Text style={{ fontSize: 20, fontWeight: "bold" }}>{user?.name}</Text>
+    <View style={{ backgroundColor: "#F7F0FC", height: 1000 }}>
+      <Header title='Profile' />
+      <View style={{ marginTop: 10, padding: 15, backgroundColor: "white" }}>
+        <View>
+          <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
+            <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+              {user?.name}
+            </Text>
+            <View
+              style={{
+                paddingHorizontal: 7,
+                paddingVertical: 5,
+                borderRadius: 8,
+                backgroundColor: "#D0D0D0",
+              }}
+            >
+              <Text>Aweera Salon</Text>
+            </View>
+          </View>
           <View
             style={{
-              paddingHorizontal: 7,
-              paddingVertical: 5,
-              borderRadius: 8,
-              backgroundColor: "#D0D0D0",
+              flexDirection: "row",
+              marginTop: 15,
+              alignItems: "center",
+              gap: 20,
             }}
           >
-            <Text>Aweera Salon</Text>
+            <View
+              style={{
+                // flex: 1,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <TouchableOpacity onPress={pickImage}>
+                <MaterialIcons
+                  name='add-photo-alternate'
+                  size={35}
+                  color='black'
+                />
+              </TouchableOpacity>
+              {image && (
+                <>
+                  <View style={{ marginRight: 20 }}>
+                    <Image
+                      source={{ uri: image }}
+                      style={{
+                        width: 100,
+                        height: 100,
+                        borderRadius: 10,
+                        margin: 10,
+                      }}
+                    />
+
+                    <TouchableOpacity
+                      style={{
+                        position: "absolute",
+                        right: 0,
+                        top: 0,
+                        borderRadius: 50,
+                        width: 30,
+                        height: 30,
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                      onPress={() => setImage(null)}
+                    >
+                      <MaterialIcons name='cancel' size={30} color='red' />
+                    </TouchableOpacity>
+                  </View>
+                </>
+              )}
+            </View>
+            <View>
+              <Image
+                style={{
+                  width: 40,
+                  height: 40,
+                  // borderradius: 20,
+                  resizeMode: "contain",
+                }}
+                source={{
+                  uri: "https://cdn-icons-png.flaticon.com/128/149/149071.png",
+                }}
+              />
+            </View>
+            <View>
+              <Text style={{ fontSize: 15, fontWeight: "400" }}>
+                Diploma in Hairdressing & Beauty Culture
+              </Text>
+              <Text style={{ fontSize: 15, fontWeight: "400" }}>
+                Full time stylist
+              </Text>
+
+              <Text style={{ fontSize: 15, fontWeight: "400" }}>
+                Experience : 2 years
+              </Text>
+            </View>
           </View>
         </View>
         <View
           style={{
-            flexDirection: "row",
-            marginTop: 15,
+            flexDirection: "raw",
             alignItems: "center",
-            gap: 20,
+            gap: 10,
+            marginTop: 20,
           }}
         >
-          <View>
-            <Image
-              style={{
-                width: 40,
-                height: 40,
-                // borderradius: 20,
-                resizeMode: "contain",
-              }}
-              source={{
-                uri: "https://cdn-icons-png.flaticon.com/128/149/149071.png",
-              }}
-            />
-          </View>
-          <View>
-            <Text style={{ fontSize: 15, fontWeight: "400" }}>
-              Diploma in Hairdressing & Beauty Culture
-            </Text>
-            <Text style={{ fontSize: 15, fontWeight: "400" }}>
-              Full time stylist
-            </Text>
-
-            <Text style={{ fontSize: 15, fontWeight: "400" }}>
-              Experience : 2 years
-            </Text>
-          </View>
+          <Pressable
+            style={{
+              // flex: 10,
+              justifyContent: "center",
+              alignItems: "center",
+              padding: 10,
+              borderColor: "#D0D0D0",
+              borderWidth: 1,
+              borderRadius: 5,
+            }}
+            onPress={editProfile}
+          >
+            <Text>Edit Profile</Text>
+          </Pressable>
+          <Pressable
+            onPress={logout}
+            style={{
+              // flex: 10,
+              justifyContent: "center",
+              alignItems: "center",
+              padding: 10,
+              borderColor: "#D0D0D0",
+              borderWidth: 1,
+              borderRadius: 5,
+            }}
+          >
+            <Text>Logout</Text>
+          </Pressable>
         </View>
-      </View>
-      <View
-        style={{
-          flexDirection: "raw",
-          alignItems: "center",
-          gap: 10,
-          marginTop: 20,
-        }}
-      >
-        <Pressable
-          style={{
-            // flex: 10,
-            justifyContent: "center",
-            alignItems: "center",
-            padding: 10,
-            borderColor: "#D0D0D0",
-            borderWidth: 1,
-            borderRadius: 5,
-          }}
-        >
-          <Text>Edit Profile</Text>
-        </Pressable>
-        <Pressable
-          onPress={logout}
-          style={{
-            // flex: 10,
-            justifyContent: "center",
-            alignItems: "center",
-            padding: 10,
-            borderColor: "#D0D0D0",
-            borderWidth: 1,
-            borderRadius: 5,
-          }}
-        >
-          <Text>Logout</Text>
-        </Pressable>
       </View>
     </View>
   );
