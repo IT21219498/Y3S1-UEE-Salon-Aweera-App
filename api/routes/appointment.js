0e4284f2b568e5e8a4619e6aa018c040ada5fb80
IT21209420 <99248793+IT21209420@users.noreply.github.com>
2023-10-20 01:00:26 +0530

    appointment Route commit

---
+++ api/routes/appointment.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const Appointment = require('../models/appointment');
+const appointmentRouter = express.Router();
+
+appointmentRouter.post('/create', async (req, res) => {
+  const newAppointment = new Appointment({
+    user: req.body.userId,
+    categoryName: req.body.category,
+    packageName: req.body.package,
+    stylistName: req.body.stylist,
+    date: req.body.date,
+    time: req.body.time,
+  });
+
+  const appointment = await newAppointment.save();
+  res.status(201).send({ message: 'New Appointment Created', appointment });
+});
+
+module.exports = appointmentRouter;
